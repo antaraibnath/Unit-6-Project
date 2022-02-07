b430987e369fcd90e7da62458bfb0c3bed009534
antaraibnath <99189344+antaraibnath@users.noreply.github.com>
2022-02-07 08:49:03 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".start-button").click(function() {
+    $(".start-button").hide();
+$(".firstImage").hide();
+    $(".title").hide();
+    $(".clue-container").text( "Peppa needs help finding her red boots to jump in muddy puddles!  Help her look for it! Where should peppa look first?");
+    $(".secondImage").show();
+    $(".Her Room!").show();
+
+});
